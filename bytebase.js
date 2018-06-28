98a22d4284c8f3d29581a8ae753e68f59c55cfd9
fishlicka <fishlicka@gmail.com>
2018-06-28 09:17:00 +0000

    changed release to endWriting with a callback cleaned/improved other things

--- bytebase.js
+++ bytebase.js
@@ -5,6 +5,7 @@ const ByteBuffer = require("bytebuffer");
 
 module.exports = ByteBase;
 function ByteBase(path) {
+    this.VERBOSE = false;
     this.writeStreams = {};
     this.tableNames = [];
     this.tableKeys = {};
@@ -21,7 +22,7 @@ function ByteBase(path) {
         try { fs.mkdirSync(dir); }
         catch (err) { if (err.code != 'EEXIST') throw(err); }
     }
-};
+}
 
 module.exports = ByteBase;
 // type enumerations
@@ -47,12 +48,11 @@ ByteBase.prototype.createTable = function(name, labels, types) {
     for (let i=1; i<labels.length; i++) o += ','+labels[i];
     for (let i=0; i<labels.length; i++) o += ','+types[i];
     fs.writeFileSync(dir+'key.txt', o);
-    console.log(o);
 };
 
 /** loads key and opens requested streams for writing or reading */
 ByteBase.prototype.initTable = function(name, writing) {
-    let dir = this.path + '/' + name + '/';
+    let dir = this.path + name + '/';
     // parse table key
     let key = fs.readFileSync(dir+'key.txt', { encoding: 'utf8' });
     let fields = key.split(',');
@@ -77,9 +77,9 @@ ByteBase.prototype.initTable = function(name, writing) {
     };
 
     // open file writer
-    if (!this.writeStreams[name]) this.writeStreams[name] = 
-        fs.createWriteStream(dir+'data.bin');
-};
+    if (writing && !this.writeStreams[name]) 
+        this.writeStreams[name] = fs.createWriteStream(dir+'data.bin');
+}
 
 /** add row(s) to table with given name */
 ByteBase.prototype.append = function(tablename, values) {
@@ -103,32 +103,45 @@ ByteBase.prototype.append = function(tablename, values) {
 };
 
 /** iterate table with given name, calling callback on rows */
-ByteBase.prototype.iterate = function(name, callback) {
+ByteBase.prototype.iterate = function(name, callback, rowOffset, numRows) {
+    let fpath = this.path + name + '/data.bin';
     let key = this.tableKeys[name];
     let data = '';
-    let readStream = fs.createReadStream(this.path+name+'/data.bin');
+
+    // create stream options reflecting rowOffset/numRows
+    let streamOptions = {};
+    if (arguments.length > 2) {
+        streamOptions.start = rowOffset * key.rowSize;
+        if (arguments.length > 3) 
+            streamOptions.end = streamOptions.start + numRows * key.rowSize;
+    }
+
+    let readStream = fs.createReadStream(
+        this.path + name + '/data.bin',
+        streamOptions);
     readStream.setEncoding('binary');
     readStream.on('data', function(chunk) {  
+        if (this.VERBOSE) console.log('CHUNK SIZE: '+chunk.length);
         data += chunk;
         let numRows = Math.floor(data.length / key.rowSize);
-        if (data.length >= key.rowSize) {
-            let rowData = Buffer.from(data.substring(0, numRows*key.rowSize), 'binary');
-            let bb = ByteBuffer.wrap(rowData);
-            for (let i=0; i<numRows; i++) {
-                let vals = [];
-                for (let j=0; j<key.types.length; j++)
-                    switch (key.types[j]) {
-                        case module.exports.TYPE_BYTE: vals.push(bb.readByte()); break;
-                        case module.exports.TYPE_INT: vals.push(bb.readInt()); break;
-                        case module.exports.TYPE_FLOAT: vals.push(bb.readFloat()); break;
-                        case module.exports.TYPE_LONG: vals.push(bb.readLong()); break;
-                    }
-                callback(vals);
-            }
+        if (numRows == 0) return 0;
+
+        let rowData = Buffer.from(data.substring(0, numRows*key.rowSize), 'binary');
+        let bb = ByteBuffer.wrap(rowData);
+        for (let i=0; i<numRows; i++) {
+            let vals = [];
+            for (let j=0; j<key.types.length; j++)
+                switch (key.types[j]) {
+                    case module.exports.TYPE_BYTE: vals.push(bb.readByte()); break;
+                    case module.exports.TYPE_INT: vals.push(bb.readInt()); break;
+                    case module.exports.TYPE_FLOAT: vals.push(bb.readFloat()); break;
+                    case module.exports.TYPE_LONG: vals.push(bb.readLong()); break;
+                }
+            callback(vals);
         }
-        data = data.substring(numRows*key.rowSize);
+        data = data.substring(numRows * key.rowSize);
     }).on('end', function() {
-        console.log('Done iterating '+name+'!');
+        if (this.VERBOSE) console.log('Done iterating '+name+'!'+' '+data.length);
         if (data.length > 0) throw "Iterating table \'%s\' finished with trailing data of incomplete row";
     });
 };
@@ -140,9 +153,10 @@ ByteBase.prototype.print = function() {
     o += 'path: ' + this.path + '\n';
     o += '--------\n';
     for (let k=0; k<this.tableNames.length; k++) {
+        o += 'table: ' + this.tableNames[k] + '\n';
         let key = this.tableKeys[this.tableNames[k]];
         for (let l=0; l<key.types.length; l++) {
-            o += key.labels[l] + '(';
+            o += ' ' + key.labels[l] + '(';
             o += types[key.types[l]] + ')\n';
         }
     }
@@ -150,11 +164,17 @@ ByteBase.prototype.print = function() {
     console.log(o);
 };
 
-/** end any open writestreams */
-ByteBase.prototype.release = function() {
+/** end open writestreams */
+ByteBase.prototype.endWriting = function(tablename, onEnd) {
+    if (tablename) {
+        if (onEnd) this.writeStreams[tablename].on('finish', onEnd);
+        this.writeStreams[tablename].end();
+        delete this.writeStreams[tablename];
+        return;
+    } // or release all tables
     let keys = Object.keys(this.writeStreams);
     for (let i=0; i<keys.length; i++) {
-        this.writeStreams[keys].end();
-        delete this.writeStreams[keys];
+        this.writeStreams[keys[i]].end();
+        delete this.writeStreams[keys[i]];
     }
 };
--- index.js
+++ index.js
@@ -2,23 +2,27 @@ var ByteBase = require('./bytebase.js');
 var bytebase = new ByteBase('database');
 
 let name = 'dogs';
-let labels = ['legs', 'weight', 'height', 'length'];
+let labels = ['legs', 'weight', 'height', 'length', 'borks'];
 let types = [ ByteBase.TYPE_INT,
               ByteBase.TYPE_FLOAT,
               ByteBase.TYPE_FLOAT,
-              ByteBase.TYPE_FLOAT ];
-bytebase.createTable(name, labels, types);
-bytebase.initTable(name, true, true);
-bytebase.print();
+              ByteBase.TYPE_FLOAT,
+              ByteBase.TYPE_FLOAT, ];
+
+  bytebase.createTable(name, labels, types);
+  bytebase.initTable(name, true);
+  bytebase.print();
 
 // populate
-for (let i=0; i<10; i++)
-    bytebase.append(name, [Math.round(Math.random()*4), Math.random()*40, Math.random()*3, Math.random()*5]);
+let pop = 10;
+for (let i=0; i<pop; i++)
+    bytebase.append(name, [i, Math.random()*40, Math.random()*3, Math.random()*5, Math.random()*9001]);
 
-// iterate and print
-bytebase.iterate(name, (vals) => {
-    for (let i=0; i<vals.length; i++)
-        console.log(labels[i]+': '+vals[i]);
+// set callback and end writing
+bytebase.endWriting(name, ()=>{
+    // iterate from dog 1 to dog 4
+    bytebase.iterate(name, (vals) => {
+        for (let i=0; i<vals.length; i++)
+            console.log(labels[i]+': '+vals[i]);
+    }, 1, 4);
 });
-
-bytebase.release();
